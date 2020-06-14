175d06d7e917790ffd962ff288c6bd28bf69e66c
yutaroyamanaka <falcon4842@gmail.com>
2020-06-14 23:27:30 +0900

    kyle audio manipulation

--- Assets/Scripts/MainCharController.cs
+++ Assets/Scripts/MainCharController.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using WebSocketSharp;
 
 public class MainCharController : MonoBehaviour
 {
     Animation anim;
 
+    private WebSocket ws_;
+
+    private string order = "";
+
     // Use this for initialization
     void Start()
     {
         anim = this.gameObject.GetComponent<Animation>();
-
+        // unity 走らせる前にNodeのサーバー建てる
+        ws_ = new WebSocket("ws://127.0.0.1:12002");
+        ws_.OnMessage += (sender, e) =>
+        {
+            order = e.Data;
+        };
+        ws_.Connect();
     }
 
     // Update is called once per frame
@@ -23,7 +34,24 @@ public class MainCharController : MonoBehaviour
         //あらかじめ設定していたintパラメーター「trans」の値を取り出す.
         int param = animator.GetInteger("param");
 
+        
+        if (order.Contains("ジャンプ"))
+        {
+            param = 1;
+            Debug.Log(param);
+        } else if(order.Contains("ストップ"))
+        {
+            param = 0;
+            Debug.Log(param);
+        } else if (order.Contains("走れ"))
+        {
+            param = -1;
+            Debug.Log(param);
+        }
+
+        /*
         //上矢印キーを押した際にパラメータ「trans」の値を増加させる.
+        
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             param++;
@@ -36,7 +64,7 @@ public class MainCharController : MonoBehaviour
             param--;
             Debug.Log(param);
         }
-
+        */
         //intパラメーターの値を設定する.
         animator.SetInteger("param", param);
 
